225184a305d341cb136bf844613d1214810776af
jun <jyunyaohmoto@gmail.com>
2025-04-15 21:16:27 +0900

    feat:乱数Mapクラスを変更
    配列も生成したいので、名前をMapに限定しない名称に変更
    配列で返す関数を追加

--- javaver/GenerateRandom.java
+++ javaver/GenerateRandom.java
@@ -2,10 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class RandMap {
+public class GenerateRandom {
     /**
      * 1～100で乱数を指定の回数生成する。
      * ソート対象なので、MapのKeyにセットする。
+     * 
      * @param roopTime ループ回数、乱数の生成数
      * @return
      */
@@ -16,5 +17,21 @@ public class RandMap {
             mapNum.put(rand.nextInt(100), i);
         }
         return mapNum;
+    }
+
+    /**
+     * 1~100の乱数を指定の回数生成する。
+     * 生成した値は配列に入れる。
+     * 
+     * @param roopTime ループ回数、乱数の生成数
+     * @return
+     */
+    public static int[] randArray(int roopTime) {
+        int[] numArray = new int[roopTime];
+        Random rand = new Random();
+        for (int i = 0; roopTime > i; i++) {
+            numArray[i] = rand.nextInt(100);
+        }
+        return numArray;
     }
 }
